be5454fe8e8025fee381126a4e1b37da4d057eb6
dangolbeeker <scottb305@outlook.com>
2020-02-11 03:41:48 -0500

    working on styles and navbar

---
+++ safefile/src/nav/nav.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+max-width: 1224px;
+width: 100%;
+margin: 0 Auto;
+height: 60px;
+padding-top: 30px;
+
+`;
+
+export const ContainerFluid = styled.div`
+padding-bottom: 10px;
+border-bottom: 2px solid rgba(0,0,0, .3);
+`
+
+// export const Toggle = styled.div`
+// display: none;
+// display
